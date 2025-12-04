9d0a16435b96d3858fc571cd3713248fbdb79ed1
UmarRabi <tipson664@gmail.com>
2025-12-04 14:58:55 +0100

    added docker files

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { CONFIG_KEYS } from './config/config.keys';
+import { getConfigValues } from './config/configuration';
+import { writeFileSync } from 'fs';
+import { Response } from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
+  const response = await getConfigValues();
+
+  console.log('secrets pulled', response);
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
@@ -12,12 +21,14 @@ async function bootstrap() {
     },
   });
 
-  app.setGlobalPrefix('api/v1');
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('operations/api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('DigiLog Admin API Documentation')
-    .setDescription('The DigiLog API description')
+    .setTitle('DigiLog Operation API Documentation')
+    .setDescription('The DigiLog Operation API description')
     .setVersion('1.0')
     .addBearerAuth(
       {
@@ -30,9 +41,18 @@ async function bootstrap() {
       'access-token', // This name is used in the decorator below
     )
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // writeFileSync(
+  //   join(process.cwd(), 'swagger.json'),
+  //   JSON.stringify(document, null, 2),
+  // );
+  // const swaggerFile = join(process.cwd(), 'swagger.json');
+  // app.getHttpAdapter().get('/operations/swagger-json', (req, res: Response) => {
+  //   res.sendFile(swaggerFile);
+  // });
+
+  SwaggerModule.setup('operations/api/docs', app, document);
+  await app.listen(configService.get<number>(CONFIG_KEYS.PORT) || 3003);
 }
 bootstrap();
